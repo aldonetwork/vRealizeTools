9c875ac999b93449d38ab1ffc78202e3022f10c2
aldonetwork <65894127+aldonetwork@users.noreply.github.com>
2020-05-25 14:03:58 +0200

    Update vrlib.php

--- middleware/vrlib.php
+++ middleware/vrlib.php
@@ -1,5 +1,16 @@
 <?php
 require_once("./config_inc.php");
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
 function vr_get_login_token(){
 	global $config;
 	if(file_exists($config["tmp_login_file"])){
